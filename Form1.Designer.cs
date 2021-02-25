2c0ede949b81970e6ecd8d63905dd3cbc8e7d2ac
Ctulhu <cry01@ya.ru>
2021-02-25 10:19:37 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace calculated
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txta = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txta
+            // 
+            this.txta.Location = new System.Drawing.Point(51, 86);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(100, 20);
+            this.txta.TabIndex = 0;
+            this.txta.TextChanged += new System.EventHandler(this.txta_TextChanged);
+            // 
+            // txtb
+            // 
+            this.txtb.Location = new System.Drawing.Point(224, 87);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(100, 20);
+            this.txtb.TabIndex = 1;
+            this.txtb.TextChanged += new System.EventHandler(this.txtb_TextChanged);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Enabled = false;
+            this.btnPlus.Location = new System.Drawing.Point(157, 86);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(29, 20);
+            this.btnPlus.TabIndex = 2;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Enabled = false;
+            this.btnMinus.Location = new System.Drawing.Point(189, 86);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(29, 20);
+            this.btnMinus.TabIndex = 3;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(330, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "=";
+            // 
+            // txtOut
+            // 
+            this.txtOut.Location = new System.Drawing.Point(349, 86);
+            this.txtOut.Name = "txtOut";
+            this.txtOut.ReadOnly = true;
+            this.txtOut.Size = new System.Drawing.Size(100, 20);
+            this.txtOut.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(167, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Calculated";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 150);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOut;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculated
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private double a, b, answer;
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            answer = a + b;
+            txtOut.Text = Convert.ToString(answer);
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            answer = a - b;
+            txtOut.Text = Convert.ToString(answer);
+        }
+
+        private void txtb_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                b = Convert.ToDouble(txtb.Text);
+                txtb.BackColor = Color.White;
+            }
+            catch
+            {
+                txtb.BackColor = Color.IndianRed;
+            }
+
+            if (txta.BackColor == Color.White && txtb.BackColor == Color.White)
+            {
+                btnMinus.Enabled = true;
+                btnPlus.Enabled = true;
+            }
+            else
+            {
+                btnMinus.Enabled = false;
+                btnPlus.Enabled = false;
+            }
+        }
+
+        private void txta_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                a = Convert.ToDouble(txta.Text);
+                txta.BackColor = Color.White;
+            }
+            catch
+            {
+                txta.BackColor = Color.IndianRed;
+            }
+
+            if (txta.BackColor == Color.White && txtb.BackColor == Color.White)
+            {
+                btnMinus.Enabled = true;
+                btnPlus.Enabled = true;
+            }
+            else
+            {
+                btnMinus.Enabled = false;
+                btnPlus.Enabled = false;
+            }
+        }
+    }
+}
